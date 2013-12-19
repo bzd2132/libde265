d75adeaf50ba22d93163afed874415261c4412b9
tom.chen <chenzhetao@gmail.com>
2013-12-20 00:46:49 +0800

    [decoder]:add aligned memory

---
+++ build/vc12-x86/hevc_memory.c
@@ -0,0 +1,21 @@
+#include "hevc_memory.h"
+
+void  hevc_aligned_free(void * _Memory)
+{
+
+}
+
+void *hevc_aligned_malloc(int _Size, int _Alignment)
+{
+
+}
+
+void *hevc_aligned_realloc(void *_Memory, int _NewSize, int _Alignment)
+{
+
+}
+
+void *hevc_aligned_recalloc(void *_Memory, int _Count, int _Size, int _Alignment)
+{
+
+}
